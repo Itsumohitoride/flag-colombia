b89df738400a8efcdfa78b1959dba2f4fa7de9a9
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-05-18 16:10:11 -0500

    The classes of the program has been added

---
+++ src/ui/Main.java
@@ -0,0 +1,14 @@
+package ui;
+
+import model.FlagMessage;
+import thread.FlagMessageThread;
+
+public class Main {
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		FlagMessage fm = new FlagMessage();
+		FlagMessageUI fmui = new FlagMessageUI();
+		FlagMessageThread fmt = new FlagMessageThread();
+	}
+}
